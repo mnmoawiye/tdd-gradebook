948397e1764c387f4d4804860dbfedc91086a671
mnmoawiye <masoudmnm21@gmail.com>
2026-04-21 08:27:00 -0400

    fixed the test_letter_grade_F

--- test_gradebook.py
+++ test_gradebook.py
@@ -3,5 +3,5 @@ from gradebook import letter_grade
 def test_letter_grade_A():
     assert letter_grade(95) == "A"
 
-def test_letter_grade_A():
+def test_letter_grade_F():
     assert letter_grade(45) == "F"
