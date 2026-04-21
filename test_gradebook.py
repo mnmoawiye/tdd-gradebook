034e19002190f8762813dbc0adbede073803acd5
mnmoawiye <masoudmnm21@gmail.com>
2026-04-21 08:37:07 -0400

    added test_letter_grade and parametrize

--- test_gradebook.py
+++ test_gradebook.py
@@ -5,3 +5,7 @@ def test_letter_grade_A():
 
 def test_letter_grade_F():
     assert letter_grade(45) == "F"
+
+@pytest.mark.parametrize("score, expected", [(95, "A"), (45, "F")])
+def test_letter_grade(score, expected):
+    assert letter_grade(score) == expected
